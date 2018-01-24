564fe8c6810f0bf2e1ab3cbed89c19f98e3db3d1
amansawhney <amansawhney@sawhneygames.com>
2018-01-23 19:13:31 -0500

    init model

--- demo.py
+++ demo.py
@@ -11,5 +11,18 @@ decay_rate = 0.99 #gradient decent
 resume = False
 
 #init model
+D = 80*80
 
+if resume:
+    model = pickle.load(open('save.p', 'rb'))
+else:
+    model = {}
+    #xavier initialization - taking in acount the hidden layer
+    #key value pair
+    #low level
+    model['W1'] = np.random.randn(H, D) / np.sqrt(D)  #makes weights not to small and not to big so it can change nicely
+    model['W2'] = np.random.randn(H) / np.sqrt(H)  #makes weights not to small and not to big so it can change nicely
+    # http://andyljones.tumblr.com/post/110998971763/an-explanation-of-xavier-initialization
+grad_buffer = { k: np.zeros_like(v) for k,v in model.iteritems()} #stores the gradient
+rmsprop_cache = {k: np.zeros_like(v) for k,v in model.iteritems()} #stores the value of a formula which is a set constent
 
