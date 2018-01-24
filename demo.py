2a006a8191832a42227a4a47ebc0ef85f8ffd47e
amansawhney <amansawhney@sawhneygames.com>
2018-01-23 19:06:26 -0500

    made factors

--- demo.py
+++ demo.py
@@ -1,3 +1,15 @@
 import numpy as np
 import cPickle as pickle
 import gym
+
+h = 200
+batch_size = 10
+learning_rate = 1e-4 #slow to converge
+gamma = 0.99 #discount factor - optimizing for the short-term rewards
+decay_rate = 0.99 #gradient decent
+#hardmode is on
+resume = False
+
+#init model
+
+
